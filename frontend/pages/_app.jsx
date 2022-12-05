e2c8b85a7e6fda8bc90d81317916b08b1a45177b
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-12-05 21:31:18 +0100

    chore: created layout skeleton component

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../lib/theme";
+import Layout from "../components/layout";
 
 const queryClient = new QueryClient();
 
@@ -9,7 +10,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme} resetCSS>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ChakraProvider>
     </QueryClientProvider>
   );
---
+++ frontend/pages/dashboard.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Dashboard() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Page
+    </div>
+  );
+}
