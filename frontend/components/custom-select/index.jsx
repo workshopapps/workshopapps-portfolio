78de9243b75555454f6314d8033f51a125604151
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-12-06 18:24:38 +0100

    chore: finished the custom select component

--- frontend/components/custom-select/index.jsx
+++ frontend/components/custom-select/index.jsx
@@ -3,17 +3,28 @@ import { HStack, Select, Text } from "@chakra-ui/react";
 
 function CustomSelect() {
   return (
-    <HStack spacing={0} rounded={8} border="1px solid" borderColor="brand.200">
-      <Text>Month</Text>
+    <HStack
+      spacing={-2}
+      rounded={8}
+      border="1px solid"
+      py={2}
+      pl={2}
+      _focusWithin={{ shadow: "outline" }}
+      borderColor="gray.300"
+    >
+      <Text color="text.200">Month:</Text>
       <Select
         placeholder="December"
         border={0}
-        // paddingEnd={-2}
         cursor="pointer"
+        color="text.100"
+        fontWeight="bold"
+        size="sm"
         outline="none"
         _focus={{ shadow: "" }}
         _active={{ shadow: "" }}
         _focusWithin={{ shadow: "" }}
+        _focusVisible={{}}
       >
         <option value="option1">Option 1</option>
         <option value="option2">Option 2</option>
