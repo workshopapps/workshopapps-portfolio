a6139e3a5130a9df5867d199ef8160890d021b4e
Fagbemi Oluwaseyi <seyifagbemi211@gmail.com>
2022-12-06 14:15:03 +0100

    styling: make tool on logo animated

--- frontend/components/layout/Navbar/Navbar.js
+++ frontend/components/layout/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import Logo from "../../assets/WorkshopAppLogo.svg";
+import Logo from "../../assets/WorkshopAppLogo";
 import HamburgerIcon from "../../assets/HamburgerIcon.svg";
 
 import styles from "./Navbar.module.css";
@@ -20,7 +20,7 @@ const Navbar = () => {
     <>
       <header className={styles.header}>
         <Link href="/">
-          <Image src={Logo} alt="logo" />
+          <Logo />
         </Link>
 
         <Image
